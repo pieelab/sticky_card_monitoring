8db1ded7c7192727510b2795b35cb1e18e67a25e
SamLokanc <SamL.5@icloud.com>
2026-06-03 12:56:16 -0700

    delete old files

--- Card.py
+++
@@ -1,5 +0,0 @@
-class Card():
-    def __init__(self, filename, crop_start_id = None):
-        self.filename = filename
-        #self.card = card
-        self.crop_start_id = crop_start_id
--- Flag.py
+++
@@ -1,6 +0,0 @@
-class Flag():
-    def __init__(self, crop, parent, bbox=[]):
-        self.crop = crop
-        self.parent = parent
-        #self.card = card
-        self.bbox = bbox
